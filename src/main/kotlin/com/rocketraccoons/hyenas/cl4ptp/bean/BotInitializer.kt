e4b3309824d2df9799b8099e3dd7390972d738d0
Igor Veyalkin <igor.veyalkin@gmail.com>
2016-04-03 21:15:10 -0400

    get rid of annoying launch messages

--- src/main/kotlin/com/rocketraccoons/hyenas/cl4ptp/bean/BotInitializer.kt
+++ src/main/kotlin/com/rocketraccoons/hyenas/cl4ptp/bean/BotInitializer.kt
@@ -1,10 +1,8 @@
 package com.rocketraccoons.hyenas.cl4ptp.bean
 
-import com.rocketraccoons.hyenas.cl4ptp.BotController
 import com.rocketraccoons.hyenas.cl4ptp.db.DatabaseClient
 import com.rocketraccoons.hyenas.cl4ptp.model.BotMessages
 import org.apache.log4j.Logger
-import java.util.*
 
 /**
  * Created by instu_000 on 3/4/2016.
@@ -16,7 +14,5 @@ class BotInitializer(val restClient: RestClient, val databaseClient: DatabaseCli
         logger.info("Retrieved successfully: $me.")
         databaseClient.setBotUser(me)
         logger.info("Bot updated user has been saved.")
-
-        restClient.sendMessage(BotController.CHAT_ID, botMessages.greetings[Random().nextInt(botMessages.greetings.size)])
     }
 }
