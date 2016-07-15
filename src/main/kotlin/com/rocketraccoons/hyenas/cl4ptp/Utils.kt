fdf8057c27eea7e2fe05a6208c9f42008fc01f38
Igor Veyalkin <igor.veyalkin@gmail.com>
2016-07-14 22:49:46 -0400

    don't append reply by default

--- src/main/kotlin/com/rocketraccoons/hyenas/cl4ptp/Utils.kt
+++ src/main/kotlin/com/rocketraccoons/hyenas/cl4ptp/Utils.kt
@@ -6,6 +6,11 @@ import com.rocketraccoons.hyenas.cl4ptp.model.UpdateSendMessagePayload
 /**
  * Created by instu_000 on 3/30/2016.
  */
-fun String.asResponseTo(message: Message) = UpdateSendMessagePayload(message.chat.id, this, message.messageId)
+fun String.asResponseTo(message: Message, appendReply: Boolean = false) =
+        UpdateSendMessagePayload(
+                message.chat.id,
+                this,
+                if (appendReply) message.messageId else null
+        )
 
 fun String.contains(vararg strings: String) = null != strings.find { this.contains(it, true) }
